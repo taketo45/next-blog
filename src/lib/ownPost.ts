cbaacd9779d0503b522fbe9e8a94efccdd084201
takezo <taketo45@gmail.com>
2025-05-30 01:11:36 +0900

    ✨ NotFoundページとShowPageコンポーネントを追加し、投稿の詳細表示機能を実装

--- src/lib/ownPost.ts
+++ src/lib/ownPost.ts
@@ -1,8 +1,10 @@
 import {prisma} from '@/lib/prisma'
 
-export async function getOwnPost(userId: string) {
+export async function getOwnPosts(userId: string) {
   return await prisma.post.findMany({
-    where: {authorId: userId},
+    where: {
+      authorId: userId
+    },
     select: {
       id: true,
       title: true,
@@ -12,3 +14,25 @@ export async function getOwnPost(userId: string) {
     orderBy: {updatedAt: "desc"}
   })
 }
+
+export async function getOwnPost(userId: string, postId: string){
+  return await prisma.post.findFirst({
+    where: { 
+      AND: [
+        { authorId: userId },
+        { id: postId }
+      ]
+    },
+    select: {
+        id: true,
+        title: true,
+        content: true,
+        topImage: true,
+        author: true,
+        published: true,
+        createdAt: true,
+        updatedAt: true
+    }
+  })
+}
+
